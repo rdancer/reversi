083ee42122cab76806ce8785b966d84a65c4b7d3
Jan Minar <rdancer@rdancer.org>
2012-01-02 19:24:31 -0800

    Add list of ignored files; HackageDB accepts the cabal package as valid

---
+++ Main.hs
@@ -0,0 +1,5 @@
+-- Main.hs -- program entry point
+
+-- Copyright 2012 Jan Minar <rdancer@rdancer.org>
+
+defaultMain = nil
---
+++ Setup.hs
@@ -0,0 +1,8 @@
+-- Setup.hs -- set up this Cabal package
+
+-- Copyright 2012 Jan Minar <rdancer@rdancer.org>
+
+-- Based on http://www.haskell.org/cabal/users-guide/
+
+import Distribution.Simple
+main = defaultMain
